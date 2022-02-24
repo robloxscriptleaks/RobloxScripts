38dd5ebc4e0ef7155e623b89710e0c811771c1e5
robloxscriptleaks <98795870+robloxscriptleaks@users.noreply.github.com>
2022-02-24 13:41:31 -0500

    Delete FlameHub.lua

--- FlameHub.lua
+++
@@ -1,9 +0,0 @@
-if game.Players.LocalPlayer.UserId == 198629 then
-	print("Access Enabled")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/robloxscriptleaks/RobloxScripts/main/FlameHub2.lua"))()
-	
-	else
-	local lol = game:GetService("RbxAnalyticsService"):GetClientId()
-			game.Players.LocalPlayer:Kick(lol..'\nYou smoking crack? Your not whitelisted for this script fucking retard.')
-	print("Denied")
-	end
